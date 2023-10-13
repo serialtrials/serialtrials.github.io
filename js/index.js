d6e01f939b436539d5cfba4d07a848e3d340e004
serialtrials <frontfinland@gmail.com>
2023-10-13 15:45:54 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,76 +1,19 @@
-// Canvas setup
-const threejsCanvas = document.querySelector('#threejs-canvas')
-let width = threejsCanvas.offsetWidth
-let height = threejsCanvas.offsetHeight
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("container").appendChild(renderer.domElement);
 
-//scene and camera setup
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000)
-camera.position.set(10, 10, 10)
-camera.lookAt(0, 0, 0)
+const loader = new THREE.OBJLoader();
+loader.load("blackdemon.obj", function (object) {
+  scene.add(object);
+});
 
-//renderer setup
-const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: true,
-})
-renderer.setSize(width, height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-threejsCanvas.appendChild(renderer.domElement)
+camera.position.z = 5;
 
-//let's add a 3D box
-const geometry = new THREE.BoxGeometry(5, 5, 5)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ffff })
-const box = new THREE.Mesh(geometry, material)
-scene.add(box)
+const animate = function () {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
 
-
-const loader = new THREE.ObjectLoader();
-
-loader.load(
-	// resource URL
-	"blackdemon.obj",
-
-	// onLoad callback
-	// Here the loaded data is assumed to be an object
-	function ( obj ) {
-		// Add the loaded object to the scene
-		scene.add( obj );
-	},
-
-	// onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
-
-	// onError callback
-	function ( err ) {
-		console.error( 'An error happened' );
-	}
-);
-
-//update
-update()
-
-
-//resize check
-window.addEventListener('resize', onResize)
-
-function update() {
-    box.rotation.x += 0.05
-    box.rotation.y += 0.01
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(update)
-}
-
-
-function onResize() {
-    width = threejsCanvas.offsetWidth
-    height = threejsCanvas.offsetHeight
-
-    renderer.setSize(width, height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-    camera.aspect = width / height
-    camera.updateProjectionMatrix()
-}
+animate();
